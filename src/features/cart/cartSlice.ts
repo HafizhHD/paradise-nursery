cc187c7d07689482666de33e54144c2cce44f89f
hafizh.hudoyo <hafizh.hudoyo@kreditpintar.com>
2024-07-17 14:16:07 +0700

    remove unnecessary comment

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -17,29 +17,10 @@ const initialState: CartSliceState = {
   value: []
 }
 
-// If you are not using async thunks you can use the standalone `createSlice`.
 export const cartSlice = createAppSlice({
   name: "cart",
-  // `createSlice` will infer the state type from the `initialState` argument
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: create => ({
-    // increment: create.reducer(state => {
-    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   // doesn't actually mutate the state because it uses the Immer library,
-    //   // which detects changes to a "draft state" and produces a brand new
-    //   // immutable state based off those changes
-    //   state.value += 1
-    // }),
-    // decrement: create.reducer(state => {
-    //   state.value -= 1
-    // }),
-    // // Use the `PayloadAction` type to declare the contents of `action.payload`
-    // incrementByAmount: create.reducer(
-    //   (state, action: PayloadAction<number>) => {
-    //     state.value += action.payload
-    //   },
-    // ),
     addItem: create.reducer(
       (state, action: PayloadAction<CartItem>) => {
         const item = state.value.find(item => item.name === action.payload.name)
@@ -66,16 +47,11 @@ export const cartSlice = createAppSlice({
       },
     ),
   }),
-  // You can define your selectors here. These selectors receive the slice
-  // state as their first argument.
   selectors: {
     selectCart: cart => cart.value
   },
 })
 
-// Action creators are generated for each case reducer function.
-export const { addItem, removeItem, deleteItem } =
-  cartSlice.actions
+export const { addItem, removeItem, deleteItem } = cartSlice.actions
 
-// Selectors returned by `slice.selectors` take the root state as their first argument.
 export const { selectCart } = cartSlice.selectors
