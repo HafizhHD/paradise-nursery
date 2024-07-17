7fea099e04c969b4e31cab657bd569bc4d00120c
hafizh.hudoyo <hafizh.hudoyo@kreditpintar.com>
2024-07-17 17:22:50 +0700

    fix: browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './features/LandingPage/LandingPage';
 import ProductListing from './features/Product/ProductListing/ProductListing';
 import ShoppingCart from './features/Product/ShoppingCart/ShoppingCart';
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="product/list" element={<ProductListing/>} />
         <Route path="product/cart" element={<ShoppingCart/>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
